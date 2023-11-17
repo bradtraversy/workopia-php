3562ebf44ff0fc13b677351f54d37929ec303c39
Brad Traversy <traversymedia@gmail.com>
2023-11-16 19:37:59 -0500

    Inspect helper functions

--- helpers.php
+++ helpers.php
@@ -47,3 +47,29 @@ function loadPartial($name)
     echo "Partial '{$name} not found!'";
   }
 }
+
+/**
+ * Inspect a value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+}
+
+/**
+ * Inspect a value(s) and die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value)
+{
+  echo '<pre>';
+  die(var_dump($value));
+  echo '</pre>';
+}
