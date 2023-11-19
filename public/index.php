cb0c47b251137c6ec184940f0118a9be2d22b0b9
Brad Traversy <traversymedia@gmail.com>
2023-11-19 12:36:48 -0500

    Custom autoloader

--- public/index.php
+++ public/index.php
@@ -1,7 +1,12 @@
 <?php
 require '../helpers.php';
-require basePath('Framework/Router.php');
-require basePath('Framework/Database.php');
+
+spl_autoload_register(function ($class) {
+  $path = basePath('Framework/' . $class . '.php');
+  if (file_exists($path)) {
+    require $path;
+  }
+});
 
 // Instatiate the router
 $router = new Router();
