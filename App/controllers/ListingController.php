4ea1cc8f440c29e1cabfac9aff09803bd7ac3d94
Brad Traversy <traversymedia@gmail.com>
2023-11-20 12:09:51 -0500

    Validation class

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use Framework\Database;
+use Framework\Validation;
 
 class ListingController
 {
---
+++ Framework/Validation.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Framework;
+
+class Validation
+{
+  /**
+   * Validate a string
+   * 
+   * @param string $value
+   * @param int $min
+   * @param int $max
+   * @return bool
+   */
+  public static function string($value, $min = 1, $max = INF)
+  {
+    if (is_string($value)) {
+      $value = trim($value);
+      $length = strlen($value);
+      return $length >= $min && $length <= $max;
+    }
+
+    return false;
+  }
+
+  /**
+   * Validate email address
+   * 
+   * @param string $value
+   * @return mixed
+   */
+  public static function email($value)
+  {
+    $value = trim($value);
+
+    return filter_var($value, FILTER_VALIDATE_URL);
+  }
+
+  /**
+   * Match a value against another
+   * @param string $value1
+   * @param string $value2
+   * @return bool
+   */
+  public static function match($value1, $value2)
+  {
+    $value1 = trim($value1);
+    $value = trim($value2);
+
+    return $value1 === $value2;
+  }
+}
